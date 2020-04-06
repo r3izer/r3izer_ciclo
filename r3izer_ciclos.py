7c6983ef1f51283f34bf166d018b5dea211035e5
r3izer <54298087+r3izer@users.noreply.github.com>
2020-04-06 13:49:46 -0400

    Add files via upload

---
+++ r3izer_ciclos.py
@@ -0,0 +1,30 @@
+j= int(input(" 0) Salir 1) iniciar : "))
+while j > 1:
+    print("Valor invalido")
+    j= int(input(" 0) Salir 1) iniciar : "))
+    if(j==0):
+        print("saliendo...")
+  
+while j ==1:
+    r = int(input("1) para un ciclo for 2) para un ciclo while : "))
+    if(r== 1):
+        for i in range(11):
+            print(i)
+    
+    if(r == 2):
+        z = 0
+        while z < 11:
+            print(z)
+            z = z +1
+        
+    j= int(input("0) Salir 1) iniciar : "))
+    if(j==0):
+        print("Saliendo...")
+    while j > 1:
+        print("Valor invalido")
+        j= int(input(" 0) Salir 1) iniciar : "))
+        if(j==0):
+            print("Saliendo...")
+        
+        
+    
